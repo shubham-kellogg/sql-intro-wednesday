0a0a556e8722597395851b75ca727f765584f8c4
Shubham Bajaj <shubham.bajaj@kellogg.northwestern.edu>
2024-01-10 16:39:45 +0000

    sql exercises

--- lab-1-3.sql
+++ lab-1-3.sql
@@ -1,5 +1,10 @@
 -- Which three teams had the losingest seasons?
 
+SELECT year, name, wins, losses 
+FROM teams
+ORDER BY losses DESC
+LIMIT 3;
+
 -- Expected result:
 --
 -- +------+----------------------+------+--------+
