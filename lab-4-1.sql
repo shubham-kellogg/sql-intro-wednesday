f2baf2c913d9c3db824bcade69f4b9cee9e903b2
Shubham Bajaj <shubham.bajaj@kellogg.northwestern.edu>
2024-01-10 17:29:22 +0000

    sql exercise 4.1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,10 @@
 -- How many lifetime hits does Barry Bonds have?
 
+SELECT SUM(stats.hits) 
+FROM players
+INNER JOIN stats ON stats.player_id = players.id
+WHERE first_name = "Barry" AND last_name = "Bonds";
+
 -- Expected result:
 -- 2935
 
