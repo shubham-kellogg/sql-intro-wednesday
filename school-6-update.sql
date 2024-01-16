3eb400eded129892fde18db87d8b3d7b27028ce2
Shubham Bajaj <shubham.bajaj@kellogg.northwestern.edu>
2024-01-16 15:07:37 +0000

    School Exercise

---
+++ school-6-update.sql
@@ -0,0 +1,3 @@
+UPDATE students
+SET phone = "202-327-3486"
+WHERE id = 1;
---
+++ school-8-insert-again.sql
@@ -0,0 +1,9 @@
+INSERT INTO students (
+    first_name, 
+    last_name, 
+    phone
+) VALUES (
+        "Shubham",
+        "Bajaj",
+        "111-222-3333"
+    )
